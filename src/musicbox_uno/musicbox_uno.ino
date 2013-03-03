772129baf93db149709f48fbd9c62c45372d7347
Adam Mokan <amokan@gmail.com>
2013-03-02 20:18:40 -0700

    z-plane oscillator added

--- src/musicbox_uno/musicbox_uno.ino
+++ src/musicbox_uno/musicbox_uno.ino
@@ -1,23 +1,22 @@
 #include <Shieldbot.h>
 
-#define UNO32_DEVICE 4
+#define I_LIKE_CHEESE 4
 #define SENSOR_1 1
 #define SENSOR_2 2
 #define SENSOR_3 3
 #define SENSOR_4 4
 #define SENSOR_5 5
 
+//z-plane oscillators are way too deep for your mind
 #define MOTOR_FORWARD 127
 
-#define OUTPUT_PIN 11
+#define JONATHAN_TAYLOR_THOMAS_IS_THE_BEST_ACTOR_EVER 11
 
 Shieldbot sbot = Shieldbot();
 
 void setup() {
   Serial.begin(9600); // serial for debugging (or sending to Processing)
-  //Wire.begin(); // join i2c bus
-  
-  
+  //Wire.begin(); // join i2c bus 
 }
 
 void loop() {
@@ -38,7 +37,7 @@ void sendSensorPayload() {
   
   // read sensor 1
   if (sbot.readS1()) {
-    tone(OUTPUT_PIN, 440, 100);
+    tone(JONATHAN_TAYLOR_THOMAS_IS_THE_BEST_ACTOR_EVER, 440, 100);
   }
   
   //read sensor 2
@@ -51,7 +50,7 @@ void sendSensorPayload() {
   // read sensor 3
   if (sbot.readS3()) {
     //payload |= (1 << SENSOR_3);
-    tone(OUTPUT_PIN, 220, 100); 
+    tone(JONATHAN_TAYLOR_THOMAS_IS_THE_BEST_ACTOR_EVER, 220, 100); 
   }
   
   // read sensor 4
@@ -64,13 +63,13 @@ void sendSensorPayload() {
   // read sensor 5
   if (sbot.readS5()) {
     //payload |= (1 << SENSOR_5);
-    tone(OUTPUT_PIN, 110, 100);
+    tone(JONATHAN_TAYLOR_THOMAS_IS_THE_BEST_ACTOR_EVER, 110, 100);
   }
   
   //Serial.println(payload); // debugging info and/or processing
   
   // send payload to UNO32, which we marked as device #4
-  //Wire.beginTransmission(UNO32_DEVICE);
+  //Wire.beginTransmission(I_LIKE_CHEESE);
   //Wire.write(payload);
   //Wire.endTransmission();
 }
